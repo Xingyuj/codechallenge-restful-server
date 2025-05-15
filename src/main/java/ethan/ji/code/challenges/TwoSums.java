ef00c83958fff477b528d4dc422bdcfe509d403d
Ethan XYJ <endorathas@gmail.com>
2025-05-16 09:36:14 +1000

    update

--- src/main/java/ethan/ji/code/challenges/TwoSums.java
+++ src/main/java/ethan/ji/code/challenges/TwoSums.java
@@ -1,7 +1,58 @@
 package ethan.ji.code.challenges;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+ * target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ *
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ *
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ */
 public class TwoSums {
+
+    public static int[] twoSum(int[] nums, int target){
+        int[] result = new int[2];
+        Map<Integer, Integer> suplementMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!suplementMap.containsKey(nums[i])) {
+                suplementMap.put(target-nums[i], i);
+            } else {
+                result[1] = i;
+                result[0] = suplementMap.get(nums[i]);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("aa");
+        System.out.println(Arrays.toString(twoSum(new int[] { 2, 7, 11, 15 }, 9)));
     }
 }
